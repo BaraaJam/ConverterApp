40d5b216a062212650a00bb2484a08253ba581b3
Baraa Jamos <Baraajamos@icloud.com>
2026-06-17 11:58:12 +0200

    Add main application loop and implement currency converter

--- src/main/java/se/lexicon/ConverterApp.java
+++ src/main/java/se/lexicon/ConverterApp.java
@@ -11,36 +11,35 @@ public class ConverterApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        boolean keepRunning = true;
-
-        while(keepRunning){
+        while(true) {
 
             printMenu();
             int selectedOption = getValidOption(input);
 
-            switch (selectedOption) {
-                case 1 :
-                    currencyConverter();
-                    break;
-                case 2 :
-                    temperatureConverter();
-                    break;
-                case 3 :
-                    lengthConverter();
-                    break;
-                case 4 :
-                    System.out.print("Continue? (yes/no): ");
-                    String doContinue = input.next();
-                    if (doContinue.equalsIgnoreCase("yes")){
-                        break;
-                    } else {
-                        System.out.print("Goodbye!");
-                        keepRunning = false;
-                    }
+            if (selectedOption == (CONVERTERS_MENU.length + 1)) {
+                System.out.print("Goodbye!");
+                break;
             }
 
-        }
+            switch (selectedOption) {
+                case 1:
+                    currencyConverter(input);
+                    break;
+                case 2:
+                    temperatureConverter();
+                    break;
+                case 3:
+                    lengthConverter();
+                    break;
+            }
 
+            System.out.print("\nContinue? (yes/no): ");
+            String doContinue = input.next();
+            if (doContinue.equalsIgnoreCase("no")) {
+                System.out.print("Goodbye!");
+                break;
+            }
+        }
         input.close();
     }
 
@@ -76,8 +75,35 @@ public class ConverterApp {
         }
     }
 
-    private static void currencyConverter(){
-        System.out.print("Currency Converter ");
+    private static void currencyConverter(Scanner input){
+        System.out.println("--- Currency Converter ---");
+        System.out.println("Converter:");
+        System.out.println("  1. SEK to USD");
+        System.out.println("  2. SEK to EUR");
+
+        while (true) {
+            System.out.print("Your choice: ");
+            int currencyChoice = input.nextInt();
+
+            if (currencyChoice == 1) {
+                System.out.print("Enter amount in SEK: ");
+                double currencyAmount = input.nextDouble();
+                double getConvertResult = currencyAmount / SEK_TO_USD_RATE;
+                System.out.printf("Result: %.2f SEK = %.2f USD\n", currencyAmount, getConvertResult);
+                break;
+
+            } else if (currencyChoice == 2) {
+                System.out.print("Enter amount in SEK: ");
+                double currencyAmount = input.nextDouble();
+                double getConvertResult = currencyAmount / SEK_TO_EUR_RATE;
+                System.out.printf("Result: %.2f SEK = %.2f EUR\n", currencyAmount, getConvertResult);
+
+                break;
+
+            } else {
+                System.out.println("Invalid option.");
+            }
+        }
     }
 
     private static void temperatureConverter(){
